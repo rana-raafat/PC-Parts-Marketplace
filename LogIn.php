59bdadf78e3273ff134e626653f3e8c3f64fda21
mohamedAyman <modyayman2001@gmail.com>
2021-12-17 20:57:47 +0200

    c

--- LogIn.php
+++ LogIn.php
@@ -12,35 +12,39 @@ if(isset($_POST['Submit'])){ //check if form was submitted
     $email=$_POST["Email"];
     if(!filter_var($email, FILTER_VALIDATE_EMAIL))//check if valid email
     {  
-        echo "Error: Please enter a valid email<br>";
+        exit("Error: Please enter a valid email") ;
     }
-    //when the user enters an invalid email then we shouldn't proceed and access the db so from $servername till the end should be inside an "else{}" statement
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "project";
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
 
-        $sql= "SELECT * FROM users WHERE email='" . $_POST['Email'] . "' AND password='" . $_POST['Password'] . "'";
-        $result = mysqli_query($conn,$sql);	
-        if (!$result) {
-            printf("Error: %s\n", mysqli_error($conn));
-            exit();
-        }
-        if($row = $result->fetch_assoc()){
-            $_SESSION["id"]=$row['id'];
-            $_SESSION["username"]=$row['username'];
-            $_SESSION["password"]=$row['password'];
-            $_SESSION["email"]=$row['email'];
-            $_SESSION["address"]=$row['address'];
-            $_SESSION["imagePath"]=$row['imagePath'];
-            $_SESSION["userType"]=$row['userType'];
-            header("Location:Home.php");
-        }
-        else{
-            echo "Invalid Email or Password";
-        }
+    else{
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "project";
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+            $sql= "SELECT * FROM users WHERE email='" . $_POST['Email'] . "' AND password='" . $_POST['Password'] . "'";
+            $result = mysqli_query($conn,$sql);	
+
+            if (!$result) {
+                printf("Error: %s\n", mysqli_error($conn));
+                exit();
+            }
+            
+            if($row = $result->fetch_assoc()){
+                $_SESSION["id"]=$row['id'];
+                $_SESSION["username"]=$row['username'];
+                $_SESSION["password"]=$row['password'];
+                $_SESSION["email"]=$row['email'];
+                $_SESSION["address"]=$row['address'];
+                $_SESSION["imagePath"]=$row['imagePath'];
+                $_SESSION["userType"]=$row['userType'];
+                header("Location:Home.php");
+            }
+            else{
+                echo "Invalid Email or Password";
+            }
+    }
 }
 ?>
 <?php include "Menu.php";?>
