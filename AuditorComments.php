29eeac790e0127e37c643569a7f7f13f3f37c747
mohamedAyman <modyayman2001@gmail.com>
2022-01-14 00:18:35 +0200

    ToBeContinued

--- AuditorComments.php
+++ AuditorComments.php
@@ -5,154 +5,193 @@ include "Menu.php";
 if($_SESSION['userType']=='auditor')
 {
     
-    $conn = new mysqli("localhost","root","", "project");
-    if(!$conn){
+    $conn_to_get_admins = new mysqli("localhost","root","", "project");
+    if(!$conn_to_get_admins){
         echo "couldn't connect to the DataBase<br>";
         die();
     }
     
     //SELECT username FROM users JOIN message ON (SenderID=users.id OR recepientID=users.id) WHERE userType="administrator"
     $auditor_sql=" SELECT * FROM message JOIN users ON (SenderID=users.id OR recepientID=users.id) WHERE userType='administrator' ";
-    $auditor_result = mysqli_query($conn,$auditor_sql);
+    $auditor_result = mysqli_query($conn_to_get_admins,$auditor_sql);
     if(!$auditor_result){
         echo "COULDN'T SEARCH FOR THE NAMES OF ADMINISTRATORS FROM THE DB<br>";
         die();
     }
-    
+
+    $unique_admin_images=array();
     $unique_admin_names=array();
     $unique_admin_IDS=array();
+    $unique_admin_usertype=array();
     while($admin_row = $auditor_result->fetch_assoc()){
         //echo $admin_row['senderID']."<br>";
                 
-                $repeated_names_OR_IDS=false;
+                $repeated_IDS=false;
 
                 for($i=0;$i<sizeof($unique_admin_names);$i++)
                 {
-                    if($unique_admin_names[$i]==$admin_row['username'])
+                    if($unique_admin_IDS[$i]==$admin_row['id'])
                     {
-                        $repeated_names_OR_IDS=true;
+                        $repeated_IDS=true;
                         break;
                     }
                 }
 
-                if($repeated_names_OR_IDS==false)
+                if($repeated_IDS==false)
                 {
+                    $unique_admin_images[]=$admin_row['imagePath'];
                     $unique_admin_names[]=$admin_row['username'];
-                    $unique_admin_IDS[]=$admin_row['id'];
+                    $unique_admin_IDS[]=$admin_row['id']; 
+                    $unique_admin_usertype[]=$admin_row['userType'];
+                   
                 }
                 
     }
-                for($i=0;$i<sizeof($unique_admin_names);$i++)
+                for($i=0;$i<sizeof($unique_admin_IDS);$i++)
                 {
+                    echo $unique_admin_images[$i]." ";
                     echo $unique_admin_names[$i]." ";
                     echo $unique_admin_IDS[$i]." ";
+                    echo $unique_admin_usertype[$i]." ";
                     
 ?>
-                <a href=AuditorComments.php?id=<?php echo $unique_admin_IDS[$i] ?>> YOU WANNA SEE MY CHATS MF? </a>
-                <br>
-<?php                            
-                }
-    if(!empty($_GET['id'])){
-
-    $sql="SELECT * FROM message JOIN users ON SenderID=users.id WHERE ( (senderID='".$_GET['id']."') OR ( recepientID='".$_GET['id']."') )";
-    $result = mysqli_query($conn,$sql);
-    if(!$result){
-    echo "COULDN'T SEARCH FOR THE NAMES OF THE PEOPLE YOU CHATED WITH FROM THE DB<br>";
-    die();
-    }
-    
-    $id_arr =array();
-    while($row =$result->fetch_assoc())
-    {
-
-        if($row['id'] != $_GET['id'])
-        {
-            //echo"hello";
-            
-            $repeated=false;
-            for($i=0;$i<sizeof($id_arr);$i++)
-            {
-                if($id_arr[$i]==$row['id'])
-                {
-                    $repeated=true;
-                    break;
-                }
-                if($repeated==false){
-                    $id_arr[]=$row['id'];
-                    ?>
-                    <!-- HERE IS HOW TO DISPLAY THE ID OF THE PERSON YOU CLICK ON  -->
-                    <h1>hello Rana </h1>
-                    <a href="AuditorComments.php?id_test=<?php echo $row['id']?>">
-                        <ul class="list-unstyled chat-list">
-                            <li class="clearfix">
-                                <img src="<?php echo $row['imagePath'];?>" alt="profile picture" width='50' height='50' class="img-circle">
-                                <div class="about">
-                                    <div class="name"> <?php echo $row['username'];?> </div>
-                                    <div class="job"><i class="fa fa-user"></i> <?php echo $row['userType']; ?> </div>                                            
-                                </div>
-                            </li>
-                        </ul>
-                    </a>
-
-                    <?php
-                }
-
-            }
-
-        }
-
-    else
-    {
-        //echo"hello";
-
-    $plist_exc_sql = "SELECT * FROM  message, users WHERE messageID=".$row['messageID']." AND users.id=".$row['recepientID'];
-
-    $plist_exc_result = mysqli_query($conn,$plist_exc_sql);
-    if(!$plist_exc_result){
-        echo "COULDN'T SEARCH from the second func";
-        die();
-    }
-
-
-        if($plist_exc_row = $plist_exc_result->fetch_assoc()){           
-            $repeated=false;
-            for($i=0;$i<sizeof($id_arr);$i++){
-                if($id_arr[$i]==$plist_exc_row['id']){
-                    $repeated=true;
-                    break;
-                }
-            }
-
-            if($repeated==false){
-                $id_arr[]=$plist_exc_row['id'];
-                ?>
-                <a href="AuditorComments.php?id_test=<?php echo $plist_exc_row['id']?>">
+                <form method="Post" action="AuditorComments.php">
+                    
+               <!-- <a href=AuditorComments.php?id=<?php //echo $unique_admin_IDS[$i] ?>> YOU WANNA SEE MY CHATS MF? </a> -->
+               <?php
+                 echo"<input type=hidden name=uAdminId value=".$unique_admin_IDS[$i].">";       
+               ?>
                 
-                    <ul class="list-unstyled chat-list">
-                        <li class="clearfix">
-                            <img src="<?php echo $plist_exc_row['imagePath'];?>" alt="profile picture" width='50' height='50' class="img-circle">
-                            <div class="about">
-                                <div class="name"> <?php echo $plist_exc_row['username'];?> </div>
-                                <div class="job"><i class="fa fa-user"></i> <?php echo $plist_exc_row['userType']; ?> </div>                                            
-                            </div>
-                        </li>
-                    </ul>
-                </a>
+                    <input type="submit" name="submitted">
+                </form>
+                <br>
+  <?php                     
+                }
+            }
 
+            if( isset($_POST['submitted']) ){
+                
+            $conn = new mysqli("localhost","root","", "project");
+            if(!$conn){
+                echo "couldn't connect to the DataBase<br>";
+                 die();
+            }
+        
+            echo "<table border='1'>
+                <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Chats</th>
+                <th>userType</th>
+                <th>ImagePath</th>
+                
+                </tr>";
+                $sql="SELECT * FROM message WHERE (senderID='".$_POST['uAdminId']."') OR ( recepientID='".$_POST['uAdminId']."')";
+                //$sql2="SELECT username, id FROM users WHERE username !='" . $_POST['username'] . "'";
+                $result = mysqli_query($conn,$sql);
+                if(!$result){
+                    echo "COULDN'T SEARCH FOR THE NAME FROM THE DB<br>";
+                    die();
+                }
+                echo "<tr>";
 
-            <?php
-
+                $unique_customer_name_arr=array(); 
+                $unique_customer_id_arr=array();
+                $unique_customer_usertype_arr=array();
+                $unique_customer_image_arr=array();
+    
+                while($row = $result->fetch_assoc())
+        {
+                    if ($row['senderID']==$_POST['uAdminId'])
+                    {
+    
+                        $unique_reciever_id="SELECT * FROM users WHERE id='" . $row['recepientID'] . "'";
+                        $fetch_unique_recieve_id = mysqli_query($conn,$unique_reciever_id);
+                        if(!$fetch_unique_recieve_id)
+                        {
+                            echo "COULDN'T SEARCH FOR THE NAME FROM THE DB<br>";
+                            die();
+                        }
+                    
+                        while($row2 = $fetch_unique_recieve_id->fetch_assoc())
+                        {
+                            $flag=false;
+                            for($i=0;$i<sizeof($unique_customer_id_arr);$i++)
+                            {
+                                if($unique_customer_id_arr[$i]==$row2['id'])
+                                {
+                                    $flag=true;
+                                    break;
+                                }
+                            }
+    
+                                if($flag==false)
+                                {
+                                    $unique_customer_name_arr[]=$row2['username'];
+                                    $unique_customer_id_arr[]=$row2['id'];
+                                    $unique_customer_usertype_arr[]=$row2['userType'];
+                                    $unique_customer_image_arr[]=$row2['imagePath'];
+                                }
+                        }             
+    
+                    }
+                    else if($row['recepientID']==$_POST['uAdminId'])
+                    {
+                        $unique_sender_id="SELECT * FROM users WHERE id='" . $row['senderID'] . "'";
+                        $row_unique_sender_id = mysqli_query($conn,$unique_sender_id);
+                        if(!$row_unique_sender_id){
+                            echo "COULDN'T SEARCH FOR THE NAME FROM THE DB<br>";
+                            die();
+                        }
+    
+                            while($row3 = $row_unique_sender_id->fetch_assoc())
+                            {   
+                                $flag=false;
+    
+                                for($i=0;$i<sizeof($unique_customer_id_arr);$i++)
+                                {
+                                    if($unique_customer_id_arr[$i]==$row3['id'])
+                                    {
+                                        $flag=true;
+                                        break;
+                                    }
+                                }
+    
+                                    if($flag==false)
+                                    {
+                                        $unique_customer_name_arr[]=$row3['username'];
+                                        $unique_customer_id_arr[]=$row3['id'];
+                                        $unique_customer_usertype_arr[]=$row3['userType'];
+                                        $unique_customer_image_arr[]=$row3['imagePath'];
+                                    } 
+    
+                            }  
+                        }
+                
+        }     
+    // mohamed mohamed mohamed hady farah hady
+            for ($i=0;$i<sizeof($unique_customer_name_arr);$i++)
+            {
+                       
+    ?>
+    <tr>
+        <td><?php echo $unique_customer_id_arr[$i]; ?></td>
+        <td><?php echo $unique_customer_name_arr[$i]; ?></td>
+        <td><a href=Chat.php?id=<?php echo $unique_customer_id_arr[$i]; ?> >Message</a></td>
+        <td><?php echo $unique_customer_usertype_arr[$i]; ?></td>
+        <td><?php echo $unique_customer_image_arr[$i]; ?></td>
+    
+    </tr> 
+    
+    <?php
+    
         }
-
+    
+        echo"</table>";
+        $conn->close(); 
     }
-    }
+    ?>          
+    
+            
 
-    }        
-}
-}
 
-else{
-    echo"you are NOT THE FATHER";
-}
-
-?>
